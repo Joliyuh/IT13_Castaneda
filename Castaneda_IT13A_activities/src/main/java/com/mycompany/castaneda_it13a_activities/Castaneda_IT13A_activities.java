404e2b6b37cce097cac5e5b78c06dce5289fe3d3
Ms. Julie <imyourjulie28@gmail.com>
2025-02-13 21:50:02 -0800

    Added Java Application

---
+++ Castaneda_IT13A_activities/src/main/java/com/mycompany/castaneda_it13a_activities/Castaneda_IT13A_activities.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.castaneda_it13a_activities;
+
+/**
+ *
+ * @author pc2
+ */
+public class Castaneda_IT13A_activities {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
